070d2ddb4e09bb57beb81255a852b416e11ca453
Jullien Lionel <lionel.jullien@hpe.com>
2017-08-25 13:45:34 +0200

    Create Upgrade iLO FW.ps1

---
+++ Powershell/iLO/Upgrade iLO FW.ps1
@@ -0,0 +1,106 @@
+   
+# -------------------------------------------------------------------------------------------------------
+# by lionel.jullien@hpe.com
+# Sept 2016
+#
+# Upgrade all iLO FW managed by the OneView Composer using iLO local account so it is required to first use the Add User to Ilo script
+#
+# OneView administrator account is required and HPE Ilo Cmdlets must be installed
+# from http://h20566.www2.hpe.com/hpsc/swd/public/readIndex?sp4ts.oid=5440658&swLangOid=8&swEnvOid=4208  
+# 
+# --------------------------------------------------------------------------------------------------------
+
+   
+#################################################################################
+#        (C) Copyright 2017 Hewlett Packard Enterprise Development LP           #
+#################################################################################
+#                                                                               #
+# Permission is hereby granted, free of charge, to any person obtaining a copy  #
+# of this software and associated documentation files (the "Software"), to deal #
+# in the Software without restriction, including without limitation the rights  #
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell     #
+# copies of the Software, and to permit persons to whom the Software is         #
+# furnished to do so, subject to the following conditions:                      #
+#                                                                               #
+# The above copyright notice and this permission notice shall be included in    #
+# all copies or substantial portions of the Software.                           #
+#                                                                               #
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR    #
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,      #
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE   #
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER        #
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, #
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN     #
+# THE SOFTWARE.                                                                 #
+#                                                                               #
+#################################################################################
+
+
+#Global variables
+$Location = "C:\\Kits\\_HP\\iLO\\iLO4\\ilo4_254.bin" # make sure to use / or \\ and not \
+$ilocreds = Get-Credential -UserName Administrator -Message "Please enter the iLO password"   
+
+
+#IP address of OneView
+$DefaultIP = "192.168.1.110" 
+Clear
+$IP = Read-Host "Please enter the IP address of your OneView appliance [$($DefaultIP)]" 
+$IP = ($DefaultIP,$IP)[[bool]$IP]
+
+# OneView Credentials
+$username = "Administrator" 
+$defaultpassword = "password" 
+$password = Read-Host "Please enter the Administrator password for OneView [$($Defaultpassword)]"
+$password = ($Defaultpassword,$password)[[bool]$password]
+
+
+# Import the OneView 3.0 library
+
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass -Force
+
+    if (-not (get-module HPOneview.310)) 
+    {  
+    Import-module HPOneview.310
+    }
+
+   
+   
+$PWord = ConvertTo-SecureString –String $password –AsPlainText -Force
+$cred = New-Object –TypeName System.Management.Automation.PSCredential –ArgumentList $Username, $PWord
+
+
+    # Connection to the Synergy Composer
+    if ((test-path Variable:ConnectedSessions) -and ($ConnectedSessions.Count -gt 1)) {
+        Write-Host -ForegroundColor red "Disconnect all existing HPOV / Composer sessions and before running script"
+        exit 1
+        }
+    elseif ((test-path Variable:ConnectedSessions) -and ($ConnectedSessions.Count -eq 1) -and ($ConnectedSessions[0].Default) -and ($ConnectedSessions[0].Name -eq $IP)) {
+        Write-Host -ForegroundColor gray "Reusing Existing Composer session"
+        }
+    else {
+        #Make a clean connection
+        Disconnect-HPOVMgmt -ErrorAction SilentlyContinue
+        $Appplianceconnection = Connect-HPOVMgmt -appliance $IP -PSCredential $cred
+        }
+
+
+import-HPOVSSLCertificate
+
+
+    $iLO4serverIPs =  Get-HPOVServer | % {$_.mpHostInfo.mpIpaddresses[1].address} # | select -first 1 
+     
+    $iLO4serverIPs  | Update-HPiLOFirmware -Credential $ilocreds -Location $Location -DisableCertificateAuthentication
+   
+    # To manually upadate an iLO, use its IP address like:
+    # "192.168.1.203" | Update-HPiLOFirmware -Credential $ilocreds -Location $Location -DisableCertificateAuthentication
+      
+   
+Write-Host "The following" $iLO4serverIPs.Count "iLOs have been updated:"
+$iLO4serverIPs  
+    
+   
+
+
+
+
+
