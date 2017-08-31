bfc9c827efcdd4b6126169f987d770b6c7c6f7e6
Jullien Lionel <lionel.jullien@hpe.com>
2017-08-31 10:58:32 +0200

    Update Remove a network.ps1

--- Powershell/Virtual Connect/Remove a network.ps1
+++ Powershell/Virtual Connect/Remove a network.ps1
@@ -2,15 +2,18 @@
 #   by lionel.jullien@hpe.com
 #   June 2016
 #
-#   This PowerShell script removes a network using a VLAN ID to a Synergy environment and unpresents this network to all Compute Modules using a Network Set.    
-#   The Virtual Connect/OneView internal name deleted by the script is always a `prefixname`+`VLAN ID` like *Production-400*.   
-#   In the background, the script removes the network created in the uplinkset and in the network set present in OneView.    
-#  
-#   This script demonstrates that with a single line of code, we can unpresent easily and quickly a VLAN to all Compute modules present in the Synergy frames managed by OneView 
+#   This PowerShell script removes a network resource from a Synergy environment and unpresents this network to all Compute Modules using a Network Set.    
+#   The network name defined in OneView/Virtual Connect deleted by this script is always a `prefixname`+`VLAN ID` like *Production-400*.   
+#   The script also removes the network resource from the LIG uplinkset and from the network set present in OneView.    
+#   
+#   This script can be use in conjunction with 'Add a network.ps1". See https://github.com/jullienl/HPE-Synergy-OneView-demos/blob/master/Powershell/Virtual Connect/Add a network.ps1
+#   
+#   With this script, you can demonstrate that with a single line of code, you can unpresent easily and quickly a network VLAN from all Compute Modules present in the Synergy frames managed by HPE OneView. 
 #        
 #   OneView administrator account is required. 
 # 
 # --------------------------------------------------------------------------------------------------------
+
    
 #################################################################################
 #        (C) Copyright 2017 Hewlett Packard Enterprise Development LP           #
