69579dbf9f2d628e56b5ef91e0935f4d323eeea6
Jullien Lionel <lionel.jullien@hpe.com>
2017-09-01 14:26:17 +0200

    Update Server_Profile_Unprovisioning_with_Image_Streamer.pp

--- Puppet/Server_Profile_Unprovisioning_with_Image_Streamer.pp
+++ Puppet/Server_Profile_Unprovisioning_with_Image_Streamer.pp
@@ -1,10 +1,6 @@
 #Vars
 $server_profile_name='PUPPET-DEMO-1'
 $server_hardware_name='Frame2-CN7515049L, bay 5'
-$server_hardware_type='SY 480 Gen9 1'
-$network_name_1='Management'
-$network_name_2='iSCSI-Deployment'
-$deployment_plan_name='RHEL-7.3-personalize-and-NIC-teamings'
 
 
 oneview_server_hardware{'ServerHardwarePowerOn':
