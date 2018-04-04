2b11d170e7ac9867239af16db75fd57e0b5a78e1
Jullien Lionel <lionel.jullien@hpe.com>
2018-04-04 17:50:04 +0200

    Update Generate a new iLO self-signed SSL certificate.ps1

--- Powershell/iLO/Generate a new iLO self-signed SSL certificate.ps1
+++ Powershell/iLO/Generate a new iLO self-signed SSL certificate.ps1
@@ -4,7 +4,7 @@ This PowerShell script generates a new self-signed SSL certificate on iLO 4 firm
 related to the advisory http://h41302.www4.hp.com/km/saw/view.do?docId=emr_na-a00042194en_us : HP Integrated Lights-Out (iLO) - 
 iLO 3 and iLO 4 Self-Signed SSL Certificate May Have an Expiration Date Earlier Than the Issued Date.
 
-After a new certificate is regenerated, the iLO restarts then the new certificated is imported into OneView and a OneView refresh takes place to 
+After a new certificate is regenerated, the iLO restarts then the new certificate is imported into OneView and a OneView refresh takes place to 
 update the status of the server using the new certificate.
 
 A RedFish REST command that was added in iLO 4 firmware 2.55 (or later) is used by this script to generate the new self-signed SSL certificate
