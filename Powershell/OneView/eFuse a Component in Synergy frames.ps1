cf14ebfe0f4463665defd9228880b3d7be2bda67
Jullien Lionel <lionel.jullien@hpe.com>
2018-05-15 11:21:13 +0200

    Update eFuse a Component in Synergy frames.ps1

--- Powershell/OneView/eFuse a Component in Synergy frames.ps1
+++ Powershell/OneView/eFuse a Component in Synergy frames.ps1
@@ -9,7 +9,9 @@
 # 
 # --------------------------------------------------------------------------------------------------------
 
-
+#
+# $enc = Get-HPOVEnclosure -Name "Frame3-CN7515049C" 
+# Reset-HPOVEnclosureDevice -Enclosure $enc  -Component Device -DeviceID 6 -Efuse -confirm:$false
 
 
 #IP address of OneView
@@ -19,14 +21,75 @@ $IP = "192.168.1.110"
 $username = "Administrator" 
 $password = "password" 
 
-# Import the OneView 3.10 library
+# Import the OneView 4.00 library
 
-Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass -Force -Confirm:$false
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass -Force -Confirm:$false 
 
-if (-not (get-module HPOneview.310)) {  
-    Import-module HPOneview.310
+
+Function MyImport-Module {
+    
+    # Import a module that can be imported
+    # If it cannot, the module is installed
+    # When -update parameter is used, the module is updated 
+    # to the latest version available on the PowerShell library
+    
+    param ( 
+        $module, 
+        [switch]$update 
+           )
+   
+   if (get-module $module -ListAvailable)
+
+        {
+        if ($update.IsPresent) 
+            {
+            # Updates the module to the latest version
+            [string]$Moduleinstalled = (Get-Module -Name $module).version
+            [string]$ModuleonRepo = (Find-Module -Name $module -ErrorAction SilentlyContinue).version
+
+            $Compare = Compare-Object $Moduleinstalled $ModuleonRepo -IncludeEqual
+
+            If (-not $Compare.SideIndicator -eq '==')
+                {
+                Update-Module -Name $module -Confirm -Force | Out-Null
+           
+                }
+            Else
+                {
+                Write-host "You are using the latest version of $module" 
+                }
+            }
+            
+        Import-module $module
+            
+        }
+
+    Else
+
+        {
+        Write-Warning "$Module is not present"
+        Write-host "`nInstalling $Module ..." 
+
+        Try
+            {
+                If ( !(get-PSRepository).name -eq "PSGallery" )
+                {Register-PSRepository -Default}
+                Install-Module –Name $module -Scope CurrentUser –Force -ErrorAction Stop | Out-Null
+            }
+        Catch
+            {
+                Write-Warning "$Module cannot be installed" 
+                $error[0] | FL * -force
+            }
+        }
+
 }
 
+#MyImport-Module PowerShellGet
+#MyImport-Module FormatPX
+#MyImport-Module SnippetPX
+MyImport-Module HPOneview.400 #-update
+#MyImport-Module PoshRSJob
    
   
 
@@ -63,16 +126,13 @@ $headers["X-API-Version"] = "300"
 $key = $ConnectedSessions[0].SessionID 
 
 $headers["auth"] = $key
-
-  
-   
+    
 
 $numberofframes = @(Get-HPOVEnclosure).count
 $frames = Get-HPOVEnclosure | % {$_.name}
-
-     
     
-clear
+    
+#clear
 
 
 #Which enclosure you want to eFuse a component?
@@ -87,13 +147,16 @@ if ($numberofframes -gt 1) {
 }
 
 if ($numberofframes -gt 2) {
-    $frameswithsatellites = $interconnects | where -Property Partnumber -EQ -value "779218-B21" 
+    $frameswithsatellites = $interconnects | where -Property Partnumber -eq "779218-B21" 
     $whosframe3 = $frameswithsatellites | group-object -Property enclosurename |  ? { $_.Count -gt 1 } | % {$_.name}  
 }
 
 if ($numberofframes -gt 3) { Write-Host "This script does not support more than 3 frames"}
 
-do {    
+
+do 
+
+{    
    
     do {
         
@@ -140,7 +203,7 @@ do {
     }
     
    
-      
+    $enclosure = Get-HPOVEnclosure | where {$_.name -Match $frame}
     $frameuuid = (Get-HPOVEnclosure | where {$_.name -Match $frame}).uuid
     $locationUri = (Get-HPOVEnclosure | where {$_.name -Match $frame}).uri
     
@@ -185,8 +248,8 @@ do {
         @{Name = "Profile"; expression = {$_.state}}        | Format-Table -AutoSize | out-host
                   
         $baynb = Read-Host "Please enter the Computer Module Bay number to efuse (1 to 12)"
-        $body = '[{"op":"replace","path":"/deviceBays/' + $baynb + '/bayPowerState","value":"E-Fuse"}]' 
-        $component = "Compute Module"  
+        #$body = '[{"op":"replace","path":"/deviceBays/' + $baynb + '/bayPowerState","value":"E-Fuse"}]' 
+        $component = "Device"  
     }
 
     if ($componenttoefuse -eq 2) {
@@ -197,8 +260,8 @@ do {
 
 
         $baynb = Read-Host "Please enter the Interconnect Module Bay number to efuse (1 to 6)"
-        $body = '[{"op":"replace","path":"/interconnectBays/' + $baynb + '/bayPowerState","value":"E-Fuse"}]'   
-        $component = "Interconnect Module"  
+        #$body = '[{"op":"replace","path":"/interconnectBays/' + $baynb + '/bayPowerState","value":"E-Fuse"}]'   
+        $component = "ICM"  
     }
 
     if ($componenttoefuse -eq 3) {
@@ -207,8 +270,8 @@ do {
         $ert | Select @{Name = "Model"; Expression = {$_.model}}, @{Name = "Bay number"; Expression = {$_.baynumber}}, @{Name = "Status"; Expression = {$_.status}} | Out-Host
         
         $baynb = Read-Host "Please enter the Appliance Bay number to efuse (1 or 2)"
-        $body = '[{"op":"replace","path":"/applianceBays/' + $baynb + '/bayPowerState","value":"E-Fuse"}]'   
-        $component = "Appliance Module"  
+        #$body = '[{"op":"replace","path":"/applianceBays/' + $baynb + '/bayPowerState","value":"E-Fuse"}]'   
+        $component = "Appliance"  
     }
 
     if ($componenttoefuse -eq 4) {
@@ -217,8 +280,8 @@ do {
         $ert | Select @{Name = "Model"; Expression = {$_.model}}, @{Name = "Bay number"; Expression = {$_.baynumber}}, @{Name = "Role"; Expression = {$_.role}}, @{Name = "Status"; Expression = {$_.status}} | Out-Host
 
         $baynb = Read-Host "Please enter the FLM Bay number to efuse (1 or 2)"
-        $body = '[{"op":"replace","path":"/managerBays/' + $baynb + '/bayPowerState","value":"E-Fuse"}]'   
-        $component = "FLM Module"  
+        #$body = '[{"op":"replace","path":"/managerBays/' + $baynb + '/bayPowerState","value":"E-Fuse"}]'   
+        $component = "FLM"  
     }
 
     if ($componenttoefuse -eq "x") { exit }
@@ -226,11 +289,16 @@ do {
 
 
     # eFuse the Component
-    $efusecomponent = Invoke-WebRequest -Uri "https://$IP/rest/enclosures/$frameuuid" -ContentType "application/json" -Headers $headers -Method PATCH -UseBasicParsing -Body $body
-    sleep 15
+    #$efusecomponent = Invoke-WebRequest -Uri "https://$IP/rest/enclosures/$frameuuid" -ContentType "application/json" -Headers $headers -Method PATCH -UseBasicParsing -Body $body
+    
+    $efusecomponent = Reset-HPOVEnclosureDevice -Enclosure $enclosure  -Component $component -DeviceID $baynb -Efuse -confirm:$false | Wait-HPOVTaskComplete
+    
+    #sleep 15
 
-    write-host ""
-    Write-Warning "The $Component in Bay $baynb is efusing!"
+    write-host `n`n`n`n
+    Write-Warning "The $Component in Bay $baynb is efusing!`n"
+
+    pause
  
 } until ( $componenttoefuse -eq "X" )
 
