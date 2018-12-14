45f3f1d4835819d7d90ebc69c81d5f89a95c85f2
Administrator@LJ.LAB <Administrator@LJ.LAB>
2018-12-14 08:50:24 +0100

    Updated for API 800 (OneView 4.1)

--- Powershell/iLO/Add user to iLO.ps1
+++ Powershell/iLO/Add user to iLO.ps1
@@ -40,23 +40,22 @@
 $DefaultIP = "192.168.1.110" 
 Clear
 $IP = Read-Host "Please enter the IP address of your OneView appliance [$($DefaultIP)]" 
-$IP = ($DefaultIP,$IP)[[bool]$IP]
+$IP = ($DefaultIP, $IP)[[bool]$IP]
 
 # OneView Credentials
 $username = "Administrator" 
 $defaultpassword = "password" 
 $password = Read-Host "Please enter the Administrator password for OneView [$($Defaultpassword)]"
-$password = ($Defaultpassword,$password)[[bool]$password]
+$password = ($Defaultpassword, $password)[[bool]$password]
 
 
-# Import the OneView 3.10 library
+# Import the OneView 4.10 library
 
 Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass -Force
 
-    if (-not (get-module HPOneview.310)) 
-    {  
-    Import-module HPOneview.310
-    }
+if (-not (get-module HPOneview.410)) {  
+    Import-module HPOneview.410
+}
 
    
    
@@ -64,19 +63,19 @@ $PWord = ConvertTo-SecureString –String $password –AsPlainText -Force
 $cred = New-Object –TypeName System.Management.Automation.PSCredential –ArgumentList $Username, $PWord
 
 
-    # Connection to the Synergy Composer
-    if ((test-path Variable:ConnectedSessions) -and ($ConnectedSessions.Count -gt 1)) {
-        Write-Host -ForegroundColor red "Disconnect all existing HPOV / Composer sessions and before running script"
-        exit 1
-        }
-    elseif ((test-path Variable:ConnectedSessions) -and ($ConnectedSessions.Count -eq 1) -and ($ConnectedSessions[0].Default) -and ($ConnectedSessions[0].Name -eq $IP)) {
-        Write-Host -ForegroundColor gray "Reusing Existing Composer session"
-        }
-    else {
-        #Make a clean connection
-        Disconnect-HPOVMgmt -ErrorAction SilentlyContinue
-        $Appplianceconnection = Connect-HPOVMgmt -appliance $IP -PSCredential $cred
-        }
+# Connection to the Synergy Composer
+if ((test-path Variable:ConnectedSessions) -and ($ConnectedSessions.Count -gt 1)) {
+    Write-Host -ForegroundColor red "Disconnect all existing HPOV / Composer sessions and before running script"
+    exit 1
+}
+elseif ((test-path Variable:ConnectedSessions) -and ($ConnectedSessions.Count -eq 1) -and ($ConnectedSessions[0].Default) -and ($ConnectedSessions[0].Name -eq $IP)) {
+    Write-Host -ForegroundColor gray "Reusing Existing Composer session"
+}
+else {
+    #Make a clean connection
+    Disconnect-HPOVMgmt -ErrorAction SilentlyContinue
+    $Appplianceconnection = Connect-HPOVMgmt -appliance $IP -PSCredential $cred
+}
 
                 
 import-HPOVSSLCertificate
@@ -84,16 +83,16 @@ import-HPOVSSLCertificate
 
 # Creation of the header
 
-    $postParams = @{userName=$username;password=$password} | ConvertTo-Json 
-    $headers = @{} 
-    #$headers["Accept"] = "application/json" 
-    $headers["X-API-Version"] = "300"
+$postParams = @{userName = $username; password = $password} | ConvertTo-Json 
+$headers = @{} 
+#$headers["Accept"] = "application/json" 
+$headers["X-API-Version"] = "800"
 
-    # Capturing the OneView Session ID and adding it to the header
+# Capturing the OneView Session ID and adding it to the header
     
-    $key = $ConnectedSessions[0].SessionID 
+$key = $ConnectedSessions[0].SessionID 
 
-    $headers["auth"] = $key
+$headers["auth"] = $key
 
 
 
@@ -110,9 +109,10 @@ clear
 
 
 if ($unsupportediLO) {
-write-host ""
-Write-warning "The following iLO(s) do not support REST API (only iLO 4 are supported) :"
-$unsupportediLO}
+    write-host ""
+    Write-warning "The following iLO(s) do not support REST API (only iLO 4 are supported) :"
+    $unsupportediLO
+}
 
 # Capture iLO User/password to create 
 $Defaultuser = "demopaq"
@@ -120,14 +120,14 @@ $Defaultuserpassword = "password"
 
 write-host ""
 $user = Read-Host "Please enter the user you want to add to all iLos [$($Defaultuser)]"
-$user = ($Defaultuser,$user)[[bool]$user]
+$user = ($Defaultuser, $user)[[bool]$user]
 
 write-host ""
 $userpassword = Read-Host "Please enter the password for $user [$($Defaultuserpassword)]"
-$userpassword = ($Defaultuserpassword,$userpassword)[[bool]$userpassword]
+$userpassword = ($Defaultuserpassword, $userpassword)[[bool]$userpassword]
 
 #Creation of the body content to pass to iLO
-$bodyiloParams = '{"UserName": "'+ $user + '", "Password": "' + $userpassword + '", "Oem": {"Hp": {"Privileges": {"RemoteConsolePriv": true, "VirtualMediaPriv": true, "UserConfigPriv": true, "iLOConfigPriv": true, "VirtualPowerAndResetPriv": true}, "LoginName": "'+ $user + '"}}}'
+$bodyiloParams = '{"UserName": "' + $user + '", "Password": "' + $userpassword + '", "Oem": {"Hp": {"Privileges": {"RemoteConsolePriv": true, "VirtualMediaPriv": true, "UserConfigPriv": true, "iLOConfigPriv": true, "VirtualPowerAndResetPriv": true}, "LoginName": "' + $user + '"}}}'
 
 # Added these lines to avoid the error: "The underlying connection was closed: Could not establish trust relationship for the SSL/TLS secure channel."
 # due to an invalid Remote Certificate
@@ -142,59 +142,55 @@ add-type -TypeDefinition  @"
             }
         }
 "@
-    [System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
+[System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
 
 
-Foreach($iloIP in $iloIPs)
-{
-# Capture of the SSO Session Key
+Foreach ($iloIP in $iloIPs) {
+    # Capture of the SSO Session Key
  
-$ilosessionkey = (Get-HPOVServer | where {$_.mpHostInfo.mpIpAddresses[1].address -eq $iloIP} | Get-HPOVIloSso -IloRestSession)."X-Auth-Token"
+    $ilosessionkey = (Get-HPOVServer | where {$_.mpHostInfo.mpIpAddresses[1].address -eq $iloIP} | Get-HPOVIloSso -IloRestSession)."X-Auth-Token"
 
-# Creation of the header using the SSO Session Key
-$headerilo = @{} 
-$headerilo["Accept"] = "application/json" 
-$headerilo["X-API-Version"] = "3"
-$headerilo["X-Auth-Token"] = $ilosessionkey 
+    # Creation of the header using the SSO Session Key
+    $headerilo = @{} 
+    $headerilo["Accept"] = "application/json" 
+    $headerilo["X-API-Version"] = "800"
+    $headerilo["X-Auth-Token"] = $ilosessionkey 
 
 
-#Creation of the user account using the hearder and body created previously
-Try {
+    #Creation of the user account using the hearder and body created previously
+    Try {
 
-$error.clear()
-$testuser = Invoke-WebRequest -Uri "https://$iloIP/rest/v1/AccountService/Accounts" -ContentType "application/json" -Headers $headerilo -Method GET -UseBasicParsing 
+        $error.clear()
+        $testuser = Invoke-WebRequest -Uri "https://$iloIP/rest/v1/AccountService/Accounts" -ContentType "application/json" -Headers $headerilo -Method GET -UseBasicParsing 
 
-if ($Error[0] -eq $Null) 
+        if ($Error[0] -eq $Null) { 
 
-   { 
+            # User account created if not present
+            if ($testuser.Content.Contains($user) -ne $True) {
+                write-host ""
+                write-host "User [$user] does not exist in iLO : $iloIP and will be created !"
 
-        # User account created if not present
-        if ($testuser.Content.Contains($user) -ne $True)
-        {
-        write-host ""
-        write-host "User [$user] does not exist in iLO : $iloIP and will be created !"
-
-        $rest = Invoke-WebRequest -Uri "https://$iloIP/rest/v1/AccountService/Accounts" -Body $bodyiloParams -ContentType "application/json" -Headers $headerilo -Method POST -UseBasicParsing
+                $rest = Invoke-WebRequest -Uri "https://$iloIP/rest/v1/AccountService/Accounts" -Body $bodyiloParams -ContentType "application/json" -Headers $headerilo -Method POST -UseBasicParsing
                 
-        write-host ""
-        Write-Host "[$user] has been created in iLo: $iloIP"
-        }
+                write-host ""
+                Write-Host "[$user] has been created in iLo: $iloIP"
+            }
         
-        # User account not created if already present
-        else
-        {
-        write-host ""
-        write-warning "User [$user] already exists in iLO : $iloIP !"
+            # User account not created if already present
+            else {
+                write-host ""
+                write-warning "User [$user] already exists in iLO : $iloIP !"
         
-        }
+            }
 
-     }
-   }
+        }
+    }
    
- #Error is returned if iLO FW is not supported
- catch [System.Net.WebException] { 
- write-host ""
- Write-Warning "The firmware of iLO: $iloIP might be too old ! [$User] has not been added !" }
+    #Error is returned if iLO FW is not supported
+    catch [System.Net.WebException] { 
+        write-host ""
+        Write-Warning "The firmware of iLO: $iloIP might be too old ! [$User] has not been added !" 
+    }
  
 }
 
