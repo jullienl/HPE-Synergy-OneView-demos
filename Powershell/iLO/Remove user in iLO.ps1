28e949b60b40ab79f9827b3d248e24ad43cb13cb
Lionel Jullien <lionel.jullien@hpe.com>
2020-04-17 12:39:06 +0200

    Update to support iLO4/5

--- Powershell/iLO/Remove user in iLO.ps1
+++ Powershell/iLO/Remove user in iLO.ps1
@@ -1,8 +1,8 @@
 # -------------------------------------------------------------------------------------------------------
 # by lionel.jullien@hpe.com
-# Appril 2020
+# April 2020
 #
-# Create a User account in iLO4/iLO5 managed by OneView without using the iLO Administrator local account
+# Delete a User account in iLO4/iLO5 managed by OneView without using the iLO Administrator local account
 #
 # OneView administrator account is required. 
 # iLO modification is done through OneView and iLO SSOsession key using REST POST method
@@ -39,9 +39,8 @@ $password = "P@ssw0rd"
 $IP = "192.168.1.110"
 
 
-# iLO User/password to create 
-$newiLOLoginName = "Ilouser"
-$newiLOPassword = "Ilouser1!"
+# iLO User to remove 
+$iLOLoginName = "Ilouser"
 
 
 Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass -Force
@@ -52,7 +51,7 @@ $credentials = New-Object System.Management.Automation.PSCredential ($username, 
 Connect-HPOVMgmt -Hostname $IP -Credential $credentials | Out-Null
 
 Clear-Host
-
+               
 import-HPOVSSLCertificate -ApplianceConnection ($connectedSessions | ? { $_.name -eq $IP })
 
 add-type -TypeDefinition  @"
@@ -83,116 +82,61 @@ write-host "`n $($iloIPs.count) iLO found : $nbilo4 x iLO4 - $nbilo5 x iLO5 " -f
 
 Foreach ($iloIP in $iloIPs) {
     # Capture of the SSO Session Key
+ 
     $ilosessionkey = ($servers | where { $_.mpHostInfo.mpIpAddresses[1].address -eq $iloIP } | Get-HPOVIloSso -IloRestSession)."X-Auth-Token"
     $iloModel = $servers | where { $_.mpHostInfo.mpIpAddresses[1].address -eq $iloIP } | % mpModel
-    
-    if ($iloModel -eq "iLO4") {
-        # creating iLO4 user object
-        # add permissions
-        $priv = @{ }
-        $priv.Add('RemoteConsolePriv', $True)
-        $priv.Add('iLOConfigPriv', $True)
-        $priv.Add('VirtualMediaPriv', $True)
-        $priv.Add('UserConfigPriv', $True)
-        $priv.Add('VirtualPowerAndResetPriv', $True)
-        # add login name
-        $hp = @{ }
-        $hp.Add('LoginName', $newiLOLoginName)
-        $hp.Add('Privileges', $priv)
-        $oem = @{ }
-        $oem.Add('Hp', $hp)
-    }
-    if ($iloModel -eq "iLO5") { 
-        # creating iLO5 user object
-        # add permissions
-        $priv = @{ }
-        $priv.Add('RemoteConsolePriv', $True)
-        $priv.Add('iLOConfigPriv', $True)
-        $priv.Add('VirtualMediaPriv', $True)
-        $priv.Add('UserConfigPriv', $True)
-        $priv.Add('VirtualPowerAndResetPriv', $True)
-        $priv.Add('HostBIOSConfigPriv', $True)
-        $priv.Add('HostNICConfigPriv', $True)
-        $priv.Add('HostStorageConfigPriv', $True)
-        # add login name
-        $hp = @{ }
-        $hp.Add('LoginName', $newiLOLoginName)
-        $hp.Add('Privileges', $priv)
-        $oem = @{ } 
-        $oem.Add('Hpe', $hp) 
-    }
 
-    # add username and password for access
-    $user = @{ }
-    $user.Add('UserName', $newiLOLoginName)
-    $user.Add('Password', $newiLOPassword)
-    $user.Add('Oem', $oem)
-
-
-    $bodyiloParams = $user | ConvertTo-Json -Depth 99
     # Creation of the header using the SSO Session Key 
-
     $headerilo = @{ } 
     $headerilo["X-Auth-Token"] = $ilosessionkey 
-
 
     Try {
 
         $error.clear()
         $users = Invoke-WebRequest -Uri "https://$iloIP/redfish/v1/AccountService/Accounts/" -Headers $headerilo -Method GET -UseBasicParsing 
-
-        # Finding all present users 
-
-        $usersarray = $users.Content | ConvertFrom-Json 
-
-        # If user to create is found in user list, flag is raised
+        
+        $accountids = (($users.Content | ConvertFrom-Json ).members)."@odata.id" -split '\r?\n'
+        
         $foundFlag = $False
 
-        foreach ($accOdataId in $usersarray.Members.'@odata.id') {
+        # Finding the account ID of our iLO User
+        foreach ($userid in $accountids) { 
+            $user = Invoke-WebRequest -Uri "https://$iloIP$userid" -Headers $headerilo -Method GET -UseBasicParsing 
 
-            $id = $accOdataId.Substring(36)
-            $acc = Invoke-WebRequest -Uri "https://$iloIP/redfish/v1/AccountService/Accounts/$id" -Headers $headerilo -Method GET -UseBasicParsing 
-            $accarray = $acc.Content | ConvertFrom-Json 
-
-            # $accarray.Username | Out-Host
-           
-            if ($accarray.Username -eq $newiLOLoginName) {
-                $foundFlag = $true
-                # Write-Host "$newiLOLoginName found!" -f Green
+            $username = ($user.Content | ConvertFrom-Json ).Username
+          
+            if ($username -eq $iLOLoginName) { 
+                $id = ($user.Content | ConvertFrom-Json ).id 
+                $foundFlag = $True
             }
+          
         }
-
-        # User account created if not present
-
-        if ($foundFlag -ne $True) {
-            
-            if ($iloModel -eq "iLO5") {
-                $rest = Invoke-WebRequest -Uri "https://$iloIP/redfish/v1/AccountService/Accounts/" -Body $bodyiloParams  -Headers $headerilo -ContentType "application/json" -Method POST -UseBasicParsing
-            }
+        #
+           
+        If ($foundFlag -eq $False) {
+            write-host "`nUser [$iLOLoginName] does not exist in iLO [$iloIP] and will not be deleted !" 
+        }
+        Else {
 
             if ($iloModel -eq "iLO4") {
-                $rest = Invoke-WebRequest -Uri "https://$iloIP/redfish/v1/AccountService/Accounts/" -Body $bodyiloParams  -Headers $headerilo -ContentType "application/json" -Method POST -UseBasicParsing
+                $rest = Invoke-WebRequest -Uri "https://$iloIP/redfish/v1/AccountService/Accounts/$id/" -Headers $headerilo -ContentType "application/json" -Method DELETE -UseBasicParsing
             }
             
-            write-host ""
-            Write-Host "[$newiLOLoginName] has been created successfuly in iLo: $iloIP" -ForegroundColor Green
+            if ($iloModel -eq "iLO5") {
+                $rest = Invoke-WebRequest -Uri "https://$iloIP/redfish/v1/AccountService/Accounts/$id" -Headers $headerilo -ContentType "application/json" -Method DELETE -UseBasicParsing
+            }
+
+            Write-Host "`n[$iLOLoginName] has been deleted successfuly in iLo [$iloIP]" -ForegroundColor Green
         }
         
-        # User account not created if already present
-        
-        else {
-
-            write-host ""
-            write-warning "User [$newiLOLoginName] already exists in iLO : $iloIP !"
-
-        }
+     
     }
 
 
     catch [System.Net.WebException] {    
         
         write-host ""
-        Write-Warning "Error ! [$newiLOLoginName] has not been added in iLO [$iloIP] !" 
+        Write-Warning "[$iLOLoginName] cannot be deleted in iLO [$iloIP] !" 
     }   
 
  
