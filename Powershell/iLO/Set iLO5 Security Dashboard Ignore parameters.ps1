7906bbb7d035e11285af553afa0d2322e06bdc33
jullienl <lionel.jullien@hpe.com>
2024-05-23 12:42:26 +0200

    Add improvements

--- Powershell/iLO/Set iLO5 Security Dashboard Ignore parameters.ps1
+++ Powershell/iLO/Set iLO5 Security Dashboard Ignore parameters.ps1
@@ -4,19 +4,23 @@
 # and sets the Security Dashboard parameters to ignore the state of a security feature as defined in the variable section  
 #
 #
-# Note: The Security Dashboard is only available on Gen10/Gen10+ servers with iLO5
+# Note: The iLO security dashboard is only available on server models Gen10 and above.
 #
 # Requirements:
 #    - PowerShell 7
 #    - HPE OneView Powershell Library
-#    - HPE iLO PowerShell Cmdlets (install-module HPEiLOCmdlets)
 #    - HPE OneView administrator account 
-#  
+  
 #
 # Output sample:
 # -------------------------------------------------------------------------------------------------------
 #
 #   Searching for servers with a security status of the system at risk. Please wait...
+#   [Frame3, bay 6 - iLO 192.168.3.195]: Testing iLO connection...
+#   [Frame3, bay 6 - iLO 192.168.3.195]: Overall security status is 'Risk'
+#   [Frame3, bay 11 - iLO 192.168.3.181]: Testing iLO connection...
+#   [Frame3, bay 11 - iLO 192.168.3.181]: Overall security status is 'Ignored'
+#   ...
 # 
 #   2 x computes have been found with the 'security status is at risk' alert:
 # 
@@ -25,8 +29,16 @@
 #   Frame3, bay 11 OK     Synergy 480 Gen10 CZ221705V1
 #   Frame3, bay 10 OK     Synergy 480 Gen10 CZ221705V7
 #
-#   [Frame3, bay 10 - iLO:192.168.0.10]: iLO security dashboard parameters changed successfully!
-#   [Frame3, bay 11 - iLO:192.168.0.2]: iLO security dashboard parameters changed successfully!
+#   [Frame3, bay 6 - iLO 192.168.3.195]: 'Require Login for iLO RBSU' iLO security parameter changed successfully to 'Ignored'!
+#   [Frame3, bay 6 - iLO 192.168.3.195]: 'Secure Boot' iLO security parameter changed successfully to 'Ignored'!            
+#   [Frame3, bay 6 - iLO 192.168.3.195]: 'Password Complexity' iLO security parameter changed successfully to 'Ignored'!    
+#   [Frame3, bay 6 - iLO 192.168.3.195]: 'Default SSL Certificate In Use' iLO security parameter changed successfully to 'Ignored'!
+#   [Frame3, bay 6 - iLO 192.168.3.195]: 'SNMPv1' iLO security parameter changed successfully to 'Ignored'!                 
+#   [Frame3, bay 7 - iLO 192.168.3.198]: 'Require Login for iLO RBSU' iLO security parameter changed successfully to 'Ignored'!
+#   [Frame3, bay 7 - iLO 192.168.3.198]: 'Secure Boot' iLO security parameter changed successfully to 'Ignored'!            
+#   [Frame3, bay 7 - iLO 192.168.3.198]: 'Password Complexity' iLO security parameter changed successfully to 'Ignored'!    
+#   [Frame3, bay 7 - iLO 192.168.3.198]: 'Default SSL Certificate In Use' iLO security parameter changed successfully to 'Ignored'!
+#   [Frame3, bay 7 - iLO 192.168.3.198]: 'SNMPv1' iLO security parameter changed successfully to 'Ignored'!  
 #
 # -------------------------------------------------------------------------------------------------------
 #
@@ -119,37 +131,55 @@ Clear-Host
 
 Write-host "Searching for servers with a security status of the system at risk. Please wait... "
 
-# Capture iLO5 server hardware managed by HPE OneView with the "Overall security status of the system is at risk" alert 
-$SHiLO5s = Search-OVIndex -Category server-hardware | ? { $_.Attributes.mpModel -eq "iLO5" } #| select -first 1
+# Capture iLO5 and 6 server hardware managed by HPE OneView with the "Overall security status of the system is at risk" alert 
+$SHiLOs = Search-OVIndex -Category server-hardware -Count 1024 | ? { $_.Attributes.mpModel -eq "iLO5" -or $_.Attributes.mpModel -eq "iLO6" } #| select -first 1
 
 $SH = @()
 
-foreach ($SHiLO5 in $SHiLO5s) {
+foreach ($SHiLO in $SHiLOs) {
     
-    $iLOIP = $SHiLO5.multiAttributes.mpIpAddresses |  ? { $_ -NotMatch "fe80" }
+    $iLOIP = $SHiLO.multiAttributes.mpIpAddresses |  ? { $_ -NotMatch "fe80" }
 
-    try {
-        $ilosessionkey = ($SHiLO5 | Get-OVIloSso -IloRestSession -SkipCertificateCheck)."X-Auth-Token"
-        $headers = @{} 
-        $headers["OData-Version"] = "4.0"
-        $headers["X-Auth-Token"] = $ilosessionkey 
+    if (! $iLOIP) {
 
-        "[0] - iLO session key: [1] " -f $iLOIP, $ilosessionkey  | Write-Verbose
-
-        $response = (Invoke-WebRequest -SkipCertificateCheck -Uri "https://$iLOIP/redfish/v1/Managers/1/SecurityService/SecurityDashboard" -Headers $headers -Method GET).content | Convertfrom-Json
-        
-        "[0] - OverallSecurityStatus: [1] " -f $iLOIP, $response.OverallSecurityStatus  | Write-Verbose
-
-        if ($response.OverallSecurityStatus -eq "Risk") {
-            $SH += $SHiLO5
-        }
-
-    }
-    catch {
-        Write-Warning "[$($SHiLO5.name)]: iLO cannot be contacted to check the security status ! Fix any communication problem you have in OneView with this iLO/server hardware !"
-        read-host
+        "[{0}]: This server's iLO has no IPv4 address assigned, skipping server..." -f $SHiLO.name
         continue
-    }  
+    }
+    else {
+        "[{0} - iLO {1}]: Testing iLO connection..." -f $SHiLO.name, $iLOIP
+        
+        $connectionStatus = Test-Connection -Ping $iloIP -quiet -Count 1
+
+        if ($false -eq $connectionStatus) {
+            "[{0} - iLO {1}]: iLO cannot be contacted, skipping server..." -f $SHiLO.name, $iLOIP
+
+        }
+        else {
+
+            try {
+                $ilosessionkey = ($SHiLO | Get-OVIloSso -IloRestSession -SkipCertificateCheck)."X-Auth-Token"
+                $headers = @{} 
+                $headers["OData-Version"] = "4.0"
+                $headers["X-Auth-Token"] = $ilosessionkey 
+
+                "iLO IP: {0} - iLO session key: {1} " -f $iLOIP, $ilosessionkey  | Write-Verbose
+
+                $response = (Invoke-WebRequest -SkipCertificateCheck -Uri "https://$iLOIP/redfish/v1/Managers/1/SecurityService/SecurityDashboard" -Headers $headers -Method GET).content | Convertfrom-Json
+        
+                "[{0} - iLO {1}]: Overall security status is '{2}'" -f $SHiLO.name, $iLOIP, $response.OverallSecurityStatus  
+
+                if ($response.OverallSecurityStatus -eq "Risk") {
+                    $SH += $SHiLO
+                }
+
+            }
+            catch {
+                Write-Warning "[$($SHiLO.name)]: iLO cannot be contacted to check the security status ! Fix any communication problem you have in OneView with this iLO/server hardware !"
+                read-host
+                continue
+            }  
+        }
+    }
 }
 
 if ($SH) {
@@ -171,46 +201,50 @@ else {
 
 
 # Creation of an object with the Security dashboard parameter
-$parametersToIgnore = @{}
+$parametersToIgnore = @()
 
 if ($IgnoreRequireLoginforiLORBSU) {
-    $parametersToIgnore["IgnoreRequireLoginforiLORBSU"] = $true
+    $parametersToIgnore += "Require Login for iLO RBSU"
 }   
 
 if ($IgnoreSecureBoot) {
-    $parametersToIgnore["IgnoreSecureBoot"] = $true
+    $parametersToIgnore += "Secure Boot"
 }
 
 if ($IgnorePasswordComplexity) {
-    $parametersToIgnore["IgnorePasswordComplexity"] = $true
+    $parametersToIgnore += "Password Complexity"
 }
 
 if ($IgnoreDefaultSSLInUse) {
-    $parametersToIgnore["IgnoreDefaultSSLInUse"] = $true
+    $parametersToIgnore += "Default SSL Certificate In Use"
+}
 
-
-}if ($IgnoreSecurityOverrideSwitch) {
-    $parametersToIgnore["IgnoreSecurityOverrideSwitch"] = $true
+if ($IgnoreSecurityOverrideSwitch) {
+    $parametersToIgnore += "Security Override Switch"
 }
 
 if ($IgnoreIPMIDCMIOverLAN) {
-    $parametersToIgnore["IgnoreIPMIDCMIOverLAN"] = $true
+    $parametersToIgnore += "IPMI/DCMI Over LAN"
 }
 
 if ($IgnoreMinimumPasswordLength) {
-    $parametersToIgnore["IgnoreMinimumPasswordLength"] = $true
+    $parametersToIgnore += "Minimum Password Length"
+}
 
-
-}if ($IgnoreAuthenticationFailureLogging) {
-    $parametersToIgnore["IgnoreAuthenticationFailureLogging"] = $true
+if ($IgnoreAuthenticationFailureLogging) {
+    $parametersToIgnore += "Authentication Failure Logging"
 }
 
 if ($IgnoreLastFirmwareScanResult) {
-    $parametersToIgnore["IgnoreLastFirmwareScanResult"] = $true
+    $parametersToIgnore += "Last Firmware Scan Result"
 }
 
 if ($IgnoreRequireHostAuthentication) {
-    $parametersToIgnore["IgnoreRequireHostAuthentication"] = $true
+    $parametersToIgnore += "Require Host Authentication"
+}
+
+if ($Ignoresnmpv1) {
+    $parametersToIgnore += "SNMPv1"
 }
 
 
@@ -218,60 +252,59 @@ if ($IgnoreRequireHostAuthentication) {
 
 Foreach ($compute in $SH) {
 
+    $iloIP = $compute.multiAttributes.mpIpAddresses |  ? { $_ -NotMatch "fe80" }
+
     # Capture of the SSO Session Key
     $iloSession = $compute  | Get-OVIloSso -IloRestSession -SkipCertificateCheck
     $ilosessionkey = $iloSession."X-Auth-Token"
 
-    $iloIP = $compute.multiAttributes.mpIpAddresses |  ? { $_ -NotMatch "fe80" }
+    "iLO IP: {0} - iLO session key: {1} " -f $iLOIP, $ilosessionkey  | Write-Verbose
 
+    # HPEiLOCmdlets do not support PowerShell 7 
     # Connection to iLO using HPEiLOCmdlets
-    $connection = Connect-HPEiLO -Address $iloIP -XAuthToken $ilosessionkey -DisableCertificateAuthentication 
-
+    # $connection = Connect-HPEiLO -Address $iloIP -XAuthToken $ilosessionkey -DisableCertificateAuthentication 
     # Modification of the security dashboard parameters using HPEiLOCmdlets
-    $connection | Enable-HPEiLOSecurityDashboardSetting @parametersToIgnore
+    # $connection | Enable-HPEiLOSecurityDashboardSetting @parametersToIgnore
+
+    # Creation of the headers  
+    $headers = @{} 
+    $headers["OData-Version"] = "4.0"
+    $headers["X-Auth-Token"] = $ilosessionkey 
+
+    # Collecting the uri security parameters
+    $body = @{}
+    $body['$expand'] = "."
     
-    # Modification of the security dashboard snmpv1 parameter using native API call
-    # Enable-HPEiLOSecurityDashboardSetting does not support ignoresnmpv1, so we need to create a native iLO API request 
-    if ($Ignoresnmpv1) {
+    $SecurityParams = Invoke-RestMethod -SkipCertificateCheck -Uri "https://$iloIP/redfish/v1/Managers/1/SecurityService/SecurityDashboard/SecurityParams" -Method Get -Headers $headers -Body $body
 
-        # Creation of the headers  
-        $headers = @{} 
-        $headers["OData-Version"] = "4.0"
-        $headers["X-Auth-Token"] = $ilosessionkey 
-   
-        # Collecting the snmpv1 uri security parameter
-        $body = @{}
-        $body['$expand'] = "."
+    foreach ($parameter in $parametersToIgnore) {
+        
+        $uri = $SecurityParams.Members | ? name -eq $parameter | % '@odata.id'
 
-        # iLO5 Redfish URI
-        $SecurityParams = Invoke-WebRequest -SkipCertificateCheck -Uri "https://$iloIP/redfish/v1/Managers/1/SecurityService/SecurityDashboard/SecurityParams" -Method Get -Headers $headers -Body $body
-        $snmp_uri = ($SecurityParams.Content | ConvertFrom-Json).Members | ? name -eq "SNMPv1" | % '@odata.id'
-
-        # Request content to ignore iLO SNMPv1 security dashboard warning
+        # Request content to ignore iLO security dashboard warning
         $body = @{}
         $body["Ignore"] = $True
         $body = $body | ConvertTo-Json  
-        # $body = ConvertTo-Json @{ Ignore =  $true } -Depth 99
-                
+
         # Method
         $method = "patch"
-
+      
         try {
-            $response = Invoke-WebRequest -SkipCertificateCheck -Uri "https://$iloIP$snmp_uri" -Body $body -ContentType "application/json" -Headers $headers -Method $method -ErrorAction Stop
-            Write-Host "[$($compute.name) - iLO:$iloIP]: iLO security dashboard parameters changed successfully!"
+            $response = Invoke-WebRequest -SkipCertificateCheck -Uri "https://$iloIP$uri" -Body $body -ContentType "application/json" -Headers $headers -Method $method -ErrorAction Stop
+            "[{0} - iLO {1}]: '$parameter' iLO security parameter changed successfully to 'Ignored'!" -f $compute.name, $iLOIP
         }
         catch {
             $err = (New-Object System.IO.StreamReader( $_.Exception.Response.GetResponseStream() )).ReadToEnd() 
             $msg = ($err | ConvertFrom-Json ).error.'@Message.ExtendedInfo'.MessageId
-            Write-Host -BackgroundColor:Black -ForegroundColor:Red "[$iloIP]: SNMPv1 security parameter modification error! Message returned: [$($msg)]"
+            Write-Host -BackgroundColor:Black -ForegroundColor:Red "[$($compute.name) - iLO $iloIP]: '$parameter' iLO security parameter modification error! Message returned: [$($msg)]"
             continue
         }
-
-        # Clearing the "Overall security status of the system is at risk" alert
-        $compute  | Get-OVAlert -AlertState Active | Where-Object { $_.description -Match "Overall security status of the system is at risk" } | Set-OVAlert -Cleared | out-null
-       
-
     }
+               
+
+    # Clearing the "Overall security status of the system is at risk" alert
+    $compute  | Get-OVAlert -AlertState Active | Where-Object { $_.description -Match "Overall security status of the system is at risk" } | Set-OVAlert -Cleared | out-null
+           
 }
 
 write-host ""
